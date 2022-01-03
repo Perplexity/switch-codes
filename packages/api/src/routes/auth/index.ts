2481e3916ae86367c9f78d85674909c0cbe5aaf3
Ryan Pearce <perplexity@outlook.com>
2022-01-03 21:42:07 +0000

    feat: bcrypt + jwt

--- packages/api/src/routes/auth/index.ts
+++ packages/api/src/routes/auth/index.ts
@@ -2,26 +2,38 @@ import { User } from '@switch-codes/common/entities'
 import { Request, Response, Router } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { createConnection } from 'typeorm'
+import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
 
 const router = Router()
 
 router.post('/', async (req: Request, res: Response) => {
+  if (!req.body.username || !req.body.password) {
+    return res.status(StatusCodes.BAD_REQUEST).send({ error: 'Invalid request' })
+  }
+  const { username, password } = req.body
   createConnection().then(async connection => {
     const userRepo = connection.getRepository(User)
 
-    const user = await userRepo.findOne({ username: req.body.username, password: req.body.password })
+    const user = await userRepo.findOne({ username })
     await connection.close()
     if (user) {
-      return res.status(StatusCodes.OK).json({
-        id: user.id,
-        username: user.username,
-        email: user.email
-      })
+      console.log('hashed', await bcrypt.hash(password, 10))
+      const validPassword = await bcrypt.compare(password, user.password)
+      if (validPassword) {
+        const userTokenObject = {
+          id: user.id,
+          username: user.username,
+          email: user.email
+        }
+        return res.status(StatusCodes.OK).json({ token: jwt.sign(userTokenObject, <string>process.env.JWT_PRIVATE_KEY) })
+      } else {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Invalid login credentials' })
+      }
     } else {
-      return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Invalid login credentials.' })
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Invalid login credentials' })
     }
   }).catch((error) => {
-    console.log(error)
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error)
   })
 })
