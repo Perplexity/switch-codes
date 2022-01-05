e9fe0791f737f371c9a5dd15e03b9db2b5aeadce
Ryan Pearce <perplexity@outlook.com>
2022-01-05 14:06:26 +0000

    feat: dashboard shit

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,13 +1,23 @@
 import { Navigate, useRoutes } from 'react-router'
 import './App.css'
-import MainLayout from './layouts/MainLayout'
+import { DashboardLayout, MainLayout } from './layouts'
+import DashboardPage from './pages/Dashboard/DashboardPage'
 import LoginPage from './pages/LoginPage'
 
-function App () {
+const App = () => {
   const routing = useRoutes([
     {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { path: ':page', element: <DashboardPage /> },
+        { path: '', element: <Navigate to="/dashboard/home" /> },
+        { path: '*', element: <Navigate to="/dashboard/home" /> }
+      ]
+    },
+    {
       path: '/',
-      element: <MainLayout/>,
+      element: <MainLayout />,
       children: [
         {
           path: 'login',
