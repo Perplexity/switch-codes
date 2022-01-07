b05cc309fbbf9e88e828b1ff0260586019fe7e53
Ryan Pearce <perplexity@outlook.com>
2022-01-07 12:25:11 +0000

    feat: button icons and sign out functionality

--- packages/web/src/components/Dashboard/NavBar.tsx
+++ packages/web/src/components/Dashboard/NavBar.tsx
@@ -1,51 +1,71 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes, faBars, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faBars, faUser, faHome, faCode, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames'
 import { useState } from 'react'
 import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import { useUser } from '../../hooks/user'
+import { Button } from '../Form'
+import { useCookies } from 'react-cookie'
+import { reset as userReset } from '../../slices/userSlice'
+import { useAppDispatch } from '../../app/hooks'
 
 const navs = [
   {
     page: 'home',
-    display: 'Home'
+    display: 'Home',
+    icon: faHome
   },
   {
     page: 'codes',
-    display: 'Codes'
+    display: 'Codes',
+    icon: faCode
   }
 ]
 
 const NavBar = () => {
   const { page } = useParams()
+  const dispatch = useAppDispatch()
+  const [, , removeCookie] = useCookies(['auth_token'])
   const [mobileOpen, setMobileOpen] = useState<boolean>(false)
   const user = useUser()
+
+  const doLogout = () => {
+    dispatch(userReset())
+    removeCookie('auth_token', { path: '/' })
+  }
+
   return (
     <div className={classNames('flex lg:static  text-center', { absolute: mobileOpen })}>
       <div className={classNames('w-72 h-screen bg-gradient-to-b from-red-500 to-red-700 lg:block text-white', { hidden: !mobileOpen })}>
         {user.loading
           ? <p>Loading...</p>
           : (
-          <>
-            <div className="flex justify-center avatar placeholder pt-4">
-              <div className="rounded-box w-24 h-24 ring ring-red-400 ring-offset-base-100 ring-offset-4">
-                <FontAwesomeIcon className='text-8xl text-white mt-6' icon={faUser} />
+            <>
+              <div className="flex justify-center avatar placeholder pt-4">
+                <div className="rounded-box w-24 h-24 ring ring-red-400 ring-offset-base-100 ring-offset-4">
+                  <FontAwesomeIcon className='text-8xl text-white mt-6' icon={faUser} />
+                </div>
               </div>
-            </div>
-            <div className='pt-4 text-xs'>
-              Signed in as {user.user?.username}
-            </div>
-          </>
+              <div className='py-4'>
+                {user.user?.username}
+              </div>
+            </>
             )}
+        <Button icon={faSignOutAlt} colour='sky' type='button' onClick={doLogout}>Sign out</Button>
         <div className='flex flex-col gap-4 pt-4'>
           {navs.map((nav, i) => {
             const active = nav.page === page
             return (
-              <div key={`nav-${i}`} className='px-2'>
+              <div key={`nav-${i}`} className='pl-2'>
                 <Link to={`/dashboard/${nav.page}`}>
                   <div className={classNames('bg-white hover:bg-sky-600 cursor-pointer', { 'bg-sky-600': active })}>
-                    <div className='bg-red-700 ml-2 p-4'>{nav.display}</div>
+                    <div className='bg-red-700 ml-2 p-4'>
+                      <div className='flex gap-2'>
+                        <FontAwesomeIcon className='text-2xl' icon={nav.icon} />
+                        {nav.display}
+                      </div>
+                    </div>
                   </div>
                 </Link>
               </div>
