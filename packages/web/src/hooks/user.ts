25427e3a9ffed652e0108ca55bad3f0758afe23f
Ryan Pearce <perplexity@outlook.com>
2022-01-07 09:37:47 +0000

    feat: created hook for retrieving user

---
+++ packages/web/src/hooks/user.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { selectUser, setLoading, setUser, UserState } from '../slices/userSlice'
+import userApi from '../api/user'
+import { UnauthorizedUser } from '../errors'
+import { useCookies } from 'react-cookie'
+
+export const useUser = (): UserState => {
+  const [, , removeCookie] = useCookies(['auth_token'])
+  const dispatch = useAppDispatch()
+  const userState = useAppSelector(selectUser)
+  const [isError, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    (async () => {
+      if (userState.loading) {
+        try {
+          const user = await userApi.getMyUser()
+          dispatch(setUser(user))
+          dispatch(setLoading(false))
+        } catch (err: any) {
+          if (err instanceof UnauthorizedUser) {
+            removeCookie('auth_token')
+          } else {
+            setError(true)
+            // CREATE API ERROR PAGE
+          }
+        }
+      }
+    })()
+  }, [])
+
+  return userState
+}
