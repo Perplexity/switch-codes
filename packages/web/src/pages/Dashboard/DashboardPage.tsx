8b416c1936062e0d22cee9f5cd7b68893b61edad
Ryan Pearce <perplexity@outlook.com>
2022-01-08 18:30:47 +0000

    feat: page stuff

--- packages/web/src/pages/Dashboard/DashboardPage.tsx
+++ packages/web/src/pages/Dashboard/DashboardPage.tsx
@@ -1,14 +1,25 @@
-import { useParams } from 'react-router'
+import { Navigate, useParams } from 'react-router'
 import { NavBar } from '../../components/Dashboard'
+import Home from './pages/Home'
+interface PageMap {
+  [index: string]: any
+}
+const pageMap: PageMap = {
+  home: Home
+}
 
 const DashboardPage = () => {
+  const params = useParams()
+  const selectedPage = params.page ?? 'home'
+  const Page = pageMap[selectedPage]
+  if (!Page) {
+    return <Navigate to='/dashboard/home' />
+  }
   return (
     <div className='bg-gray-50'>
       <div className='flex flex-row gap-4'>
-          <NavBar />
-        <div>
-          <p>BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-        </div>
+        <NavBar />
+        <Page />
       </div>
     </div>
   )
