db9699ba7a7144e99f46cc3245fdd2e217e2f75c
Ryan Pearce <perplexity@outlook.com>
2022-01-15 19:40:10 +0000

    fixtures as functions

---
+++ packages/web/src/spec/fixtures/useNews.ts
@@ -0,0 +1,18 @@
+import moment from 'moment'
+import { NewsState } from '../../slices/newsSlice'
+
+export const getUseNewsData = (loading: boolean = false): NewsState => {
+  return {
+    loading,
+    news: loading
+      ? []
+      : [
+          {
+            id: 1,
+            title: 'Welcome',
+            content: 'Hello and welcome to the website, this is just a test.',
+            timestamp: moment().subtract(5, 'minutes').toDate()
+          }
+        ]
+  }
+}
