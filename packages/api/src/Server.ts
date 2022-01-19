2b8a93a7d393bfda08b35448a901df3fa1f69988
Ryan Pearce <perplexity@outlook.com>
2022-01-19 22:54:40 +0000

    added google to script src

--- packages/api/src/Server.ts
+++ packages/api/src/Server.ts
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'prod') {
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", 'google.com'],
         styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
         imgSrc: ["'self'", 'data:'],
         fontSrc: ["'self'", 'https:', 'data:']
